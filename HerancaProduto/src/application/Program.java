f90f468c990ea3b639339093aff28905ec449354
jvitords <jvduarte.santos@hotmail.com>
2024-11-29 09:34:17 -0300

    Ajuste na data do produto

--- HerancaProduto/src/application/Program.java
+++ HerancaProduto/src/application/Program.java
@@ -1,8 +1,8 @@
 package application;
 
 import java.text.ParseException;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Scanner;
 
 import entities.Produto;
@@ -18,13 +18,17 @@ public class Program {
 		Produto produto = new Produto("Tenis", 300.0);
 		Produto produtoImportado = new ProdutoImportado("TV", 1200.0, 50.0);
 		
-		LocalDate data = LocalDate.parse(digitar.next(), DateTimeFormatter.ofPattern("dd//MM/yyyy"));
-		
+		SimpleDateFormat formatoDaData = new SimpleDateFormat("dd/MM/yyyy");
+		System.out.print("Data do contrato: ");
+		Date data = formatoDaData.parse(digitar.next());
+		System.out.println("Data: " + formatoDaData.format(data));
+		 
 		ProdutoUsado produtoUsado = new ProdutoUsado("Carro", 30000.0, data);
 		
 		System.out.println(produto.etiqueta());
 		System.out.println(produtoImportado.etiqueta());
 		System.out.println(produtoUsado.etiqueta());
 		
+		digitar.close();
 	}
 }
--- HerancaProduto/src/entities/ProdutoUsado.java
+++ HerancaProduto/src/entities/ProdutoUsado.java
@@ -1,22 +1,25 @@
 package entities;
 
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.Date;
 
 public class ProdutoUsado extends Produto{
 
-	private LocalDate dataDaCompra;
+	private Date dataDaCompra;
 	
 	public ProdutoUsado() {
 	}
 
-	public ProdutoUsado(String nome, Double preco, LocalDate dataDaCompra) {
+	public ProdutoUsado(String nome, Double preco, Date dataDaCompra) {
 		super(nome, preco);
 		this.dataDaCompra = dataDaCompra;
 	}
 
 	@Override
 	public String etiqueta() {
-		return getNome() + " (usado) - R$" + getPreco() + " (fabricado: " + dataDaCompra.format(DateTimeFormatter.ofPattern("dd/MM/yyyy")) + ")";
+		SimpleDateFormat formatoDaData = new SimpleDateFormat("dd/MM/yyyy");
+		return getNome() + " (usado) - R$" + getPreco() + " (fabricado: " + formatoDaData.format(dataDaCompra) + ")";
 	}
 }
